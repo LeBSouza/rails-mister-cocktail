041ac012449d13b9af80b54a16fd4dab3873e585
Leandro Souza <cartaparaleandro@gmail.com>
2019-02-15 16:26:27 -0200

    working in progress, basic backend, ugly SCSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :cocktails, only: [ :index, :show, :new, :create ] do
-    resources :doses, only: [ :new, :create, :destroy ]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root to: 'cocktails#root'
+
+  resources :cocktails, only: [:index, :show, :create, :new, :destroy] do
+    resources :doses, only: [:new, :create]
   end
 
+  resources :doses, only: [:destroy]
   # resources :ingredients, only: [ :index, :show, :new, :create ]
   # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating ingredients...'
+
+5.times do
+  ingredients_attributes =
+    { name: Faker::Food.ingredient }
+  Ingredient.create!(ingredients_attributes)
+end
+
+5.times do
+  ingredients_attributes =
+    { name: Faker::Food.spice }
+  Ingredient.create!(ingredients_attributes)
+end
+
+5.times do
+  ingredients_attributes =
+    { name: Faker::Food.fruits }
+  Ingredient.create!(ingredients_attributes)
+end
+
+puts 'Finished!'
